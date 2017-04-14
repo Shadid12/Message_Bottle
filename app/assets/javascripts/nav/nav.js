6e77e797294273c2d80fbcc89ee6a1220df7d51a
Shadid12 <shadid.haque@uoit.net>
2017-04-14 06:44:53 +0000

    done adding css

--- app/assets/javascripts/nav/nav.js
+++ app/assets/javascripts/nav/nav.js
@@ -1,5 +1,35 @@
 /*global $*/
     $(document).ready(function(){
+        
+        $("#signup_card").hide();
+        $("#login_card").hide();
+        
+        $("#home").click(function(){
+           setTimeout(function(){
+               $("#header").slideDown("slow");
+           }, 500);
+           $("#signup_card").fadeOut(500);
+           $("#login_card").fadeOut(500);
+        });
+        
+        $("#signup_col").click(function() {
+            setTimeout( function(){
+                $('#signup_card').slideDown("slow");
+            }, 700);
+            $("#header").fadeOut(500);
+            $("#login_card").fadeOut(500);
+        });
+        
+        $("#login_col").click(function() {
+            setTimeout( function(){
+                $('#login_card').slideDown("slow");
+            }, 700);
+            $("#header").fadeOut(500);
+            $("#signup_card").fadeOut(500);
+        });
+    
+    
+    // sign up form submmit     
        $("#submit").click(function(){
            var name = $("#name").val();
            var email = $("#email").val();
