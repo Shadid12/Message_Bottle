22525c1a300da30b7a2a0ee75a72dc13d412a1d0
Shadid12 <shadid.haque@uoit.net>
2017-04-15 10:25:18 +0000

    working minimal login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
+        log_in @user
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
